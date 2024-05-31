373b2ba3157b82d4c509f9c3c6a1abd99341be75
GrKoR <dr.g.kornev@gmail.com>
2024-05-31 23:23:27 +0300

    readme updated, board_ver++

--- include/About.h
+++ include/About.h
@@ -5,7 +5,7 @@ namespace About
 	static constexpr char name[] = "BackLightECU";
 	static constexpr char desc[] = "Back light control board for Pixel project";
 	static constexpr char board_type = Consts::BOARD_TYPE_BACKLIGHT;		// 5 bits
-	static constexpr char board_ver = 1;		// 3 bits
+	static constexpr char board_ver = 2;		// 3 bits
 	static constexpr char soft_ver = 1;			// 6 bits
 	static constexpr char can_ver = 1;			// 2 bits
 	static constexpr char git[] = "https://github.com/starfactorypixel/SlaveECU_BackLight";
